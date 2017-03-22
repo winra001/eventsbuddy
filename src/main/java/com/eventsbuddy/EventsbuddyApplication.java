fc517150258b28772ba2d279b15a5f33fae20fb3
daniel ra <winra001@hotmail.com>
2017-03-22 21:18:31 +1100

    Init

---
+++ src/main/java/com/eventsbuddy/EventsbuddyApplication.java
@@ -0,0 +1,12 @@
+package com.eventsbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsbuddyApplication.class, args);
+	}
+}
